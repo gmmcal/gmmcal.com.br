95762a06143a8a66a67a1a54db21951ad5856e8d
Gustavo Cunha <gustavocunha@gmail.com>
2016-06-13 10:12:00 +0100

    Remove stage config files (not needed)

--- config/deploy/stage.rb
+++
@@ -1,7 +0,0 @@
-role :app, "#{fetch(:user)}@#{fetch(:server)}"
-role :web, "#{fetch(:user)}@#{fetch(:server)}"
-role :db,  "#{fetch(:user)}@#{fetch(:server)}"
-set :stage, :stage
-set :rvm_ruby_version, "#{fetch(:ruby_version)}@stage.#{fetch(:ruby_gemset)}"
-# Default deploy_to directory is /var/www/my_app
-set :deploy_to, "#{fetch(:base_path)}/stage"
