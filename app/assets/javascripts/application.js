5a557118eb26a207cfe84ffb091d51f1d5738dc4
Gustavo Cunha <gustavocunha@gmail.com>
2016-07-20 20:41:22 +0100

    Fixing CSS/JS issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -94,7 +94,7 @@ jQuery(function($) {
 			}
 		})
 
-		$('a[href*=#]:not([href=#])').click(function() {
+		$('a[href*="#"]:not([href="#"])').click(function() {
 			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 				var target = $(this.hash);
 				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
