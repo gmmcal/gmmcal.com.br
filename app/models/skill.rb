79eb0727f8f81be847e6f3d6332e71391594d7df
Gustavo Cunha <gustavocunha@gmail.com>
2017-10-07 20:02:42 +0100

    Add constraint to value on Skill

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,7 +3,8 @@ class Skill < ApplicationRecord
 
   validates :name, presence: true
   validates :hero, presence: true
-  validates :value,  presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :value,  presence: true,
+    numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 100}
   validates :locale, presence: true
   validates :contentful_id, presence: true
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Skill, type: :model do
   it { should validate_presence_of(:hero) }
   it { should validate_presence_of(:value) }
   it { should validate_numericality_of(:value).is_greater_than_or_equal_to(0) }
+  it { should validate_numericality_of(:value).is_less_than_or_equal_to(100) }
   it { should validate_presence_of(:locale) }
   it { should validate_presence_of(:contentful_id) }
 end
