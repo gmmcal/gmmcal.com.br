1c81a8f23351277669f4bee59a0cc92c62435aa9
Gustavo Cunha <gustavocunha@gmail.com>
2017-10-16 10:18:28 +0100

    Fix include issue

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,6 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'shoulda/matchers'
-require 'shared/models'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
