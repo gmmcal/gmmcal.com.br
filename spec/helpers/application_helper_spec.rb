2330513908b2230357429667b10f6cbe3369ff2b
Gustavo Cunha <gustavocunha@gmail.com>
2017-10-17 09:09:52 +0100

    Enforce default locale on helper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
+  before(:all) do
+    I18n.locale = I18n.default_locale
+  end
+
   describe '#ldate' do
     it 'with a date, returns the formatted date' do
       date = Date.new(2017, 01, 01)
