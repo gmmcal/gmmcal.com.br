a9612aa1690b36c734109e48b36675a9ada2b9c4
Gustavo Cunha <gustavocunha@gmail.com>
2017-10-19 19:40:35 +0100

    Fix rubocop offenses

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,5 +2,5 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  scope :with_locale, -> (locale) { where(locale: locale) }
+  scope :with_locale, ->(locale) { where(locale: locale) }
 end
