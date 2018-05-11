306577ffb9db167448f04368196dddca219b4d40
Gustavo Cunha <gustavocunha@gmail.com>
2018-05-11 14:04:07 +0200

    JS stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,40 +1,39 @@
-/* eslint no-console:0 */
-window.$ = window.jQuery = require('jquery/dist/jquery')
-require('bootstrap/dist/js/bootstrap')
-require('waypoints/lib/jquery.waypoints')
-require('isotope-layout/dist/isotope.pkgd')
-require('ekko-lightbox/dist/ekko-lightbox')
-require('jquery.easing/jquery.easing')
-require('easy-pie-chart/dist/jquery.easypiechart')
-require('./jquery.gridrotator')
-require('./jquery.placeholder')
+window.$ = window.jQuery = require('jquery/dist/jquery');
+require('bootstrap/dist/js/bootstrap');
+require('waypoints/lib/jquery.waypoints');
+require('isotope-layout/dist/isotope.pkgd');
+require('ekko-lightbox/dist/ekko-lightbox');
+require('jquery.easing/jquery.easing');
+require('easy-pie-chart/dist/jquery.easypiechart');
+require('./jquery.gridrotator');
+require('./jquery.placeholder');
 
 document.addEventListener('DOMContentLoaded', function () {
-  const $ = window.jQuery
-  const $bg = $('.banner-img')
+  const $ = window.jQuery;
+  const $bg = $('.banner-img');
 
   $(window).resize(() => {
     // for banner height js
-    const windowWidth = $(window).width()
-    const windowHeight = $(window).height()
+    const windowWidth = $(window).width();
+    const windowHeight = $(window).height();
     $('.banner').css({
       'width': windowWidth,
       'height': windowHeight - '60'
-    })
+    });
     if ($(window).width() < $(window).height()) {
       $bg
         .removeClass()
-        .addClass('bgheight')
+        .addClass('bgheight');
     } else {
       $bg
         .removeClass()
-        .addClass('bgwidth')
+        .addClass('bgwidth');
     }
-  }).trigger('resize')
+  }).trigger('resize');
 
-  let index = 0
+  let index = 0;
   $(document).on('scroll', () => {
-    const top = $('.technical').height() - $(window).scrollTop()
+    const top = $('.technical').height() - $(window).scrollTop();
     if (top < -300) {
       if (index === 0) {
         $('.chart').easyPieChart({
@@ -45,42 +44,42 @@ document.addEventListener('DOMContentLoaded', function () {
           lineWidth: 15,
           size: 152,
           onStep (from, to, percent) {
-            $(this.el).find('.percent').text(Math.round(percent))
+            $(this.el).find('.percent').text(Math.round(percent));
           }
-        })
+        });
       }
-      index++
+      index++;
     }
-  })
+  });
 
   $('#home, #aboutme, #technical, #experience, #education, #portfolio, #contact, #footer').waypoint({
     handler (direction) {
-      $('.navbar-wrapper li').removeClass('active')
-      const link = $(`a[href='#${$(this.element).attr('id')}'`)
-      link.parent().addClass('active')
+      $('.navbar-wrapper li').removeClass('active');
+      const link = $(`a[href='#${$(this.element).attr('id')}'`);
+      link.parent().addClass('active');
       /* eslint-disable no-undef */
       ga('send', 'pageview', {
         'page': $(link).attr('href'),
         'title': $(link).text()
-      })
+      });
       /* eslint-enable no-undef */
     },
     offset: $('.navbar-wrapper').height()
-  })
+  });
 
   $('.navbar.navbar-inverse.navbar-static-top a').click(() => {
-    $('.navbar-collapse').addClass('hide-class')
-  })
+    $('.navbar-collapse').addClass('hide-class');
+  });
 
   $('.navbar.navbar-inverse.navbar-static-top a').click(() => {
-    $('.navbar-collapse').addClass('collapse')
-  })
+    $('.navbar-collapse').addClass('collapse');
+  });
 
   $('.navbar.navbar-inverse.navbar-static-top a').click(() => {
-    $('.navbar-collapse').removeClass('in')
-  })
+    $('.navbar-collapse').removeClass('in');
+  });
 
   $('.navbar-toggle').click(() => {
-    $('.navbar-collapse').removeClass('hide-class')
-  })
-})
+    $('.navbar-collapse').removeClass('hide-class');
+  });
+});
