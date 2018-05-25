55c18bcdc89b9c4544ffa2c906c254b4c9fa719c
Gustavo Cunha <gustavocunha@gmail.com>
2018-05-25 15:43:35 +0200

    Update webpack

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,3 +1,5 @@
-const environment = require('./environment');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-module.exports = environment.toWebpackConfig();
+const environment = require('./environment')
+
+module.exports = environment.toWebpackConfig()
