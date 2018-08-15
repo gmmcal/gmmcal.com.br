f733dbed1a71098be42c8c303b630c48e69c8311
Gustavo Cunha <gustavocunha@gmail.com>
2018-08-15 15:42:36 +0200

    Some improvements after Google Chrome Audit review

--- app/javascript/packs/components/about.vue
+++ app/javascript/packs/components/about.vue
@@ -5,7 +5,7 @@
         <h2>{{ I18n.t('title', { scope: 'about' }) }}</h2>
       </div>
       <div class="row">
-        <div class=" col-xs-12 col-sm-12 col-md-7 col-lg-7">
+        <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
           <h3>{{ I18n.t('name', { scope: 'template' }) }}</h3>
           <h4 class="sub-heading">{{ about.job_title }}</h4>
           <vue-markdown>{{ about.description }}</vue-markdown>
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,6 +113,6 @@ Rails.application.configure do
   # Use a different cache store in production.
   config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    'Cache-Control' => "public, max-age=#{1.day.to_i}"
   }
 end
