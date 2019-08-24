191a2f01b422852499e9399438e42e7fbf4b2b20
Gustavo Cunha <github@gustavocunha.dev>
2019-08-24 19:11:28 +0200

    Fix CORS lookoup

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if defined?(Rack::Cors) && ENV['CORS_HOSTS']
+if defined?(Rack::Cors) && ENV['CORS_HOSTS'].present?
   Rails.configuration.middleware.insert_before 0, Rack::Cors do
     allow do
       origins ENV['CORS_HOSTS'].split(',')
