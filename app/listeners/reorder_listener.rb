2eda6f8606c1eb287fbc9c70acd7eb30fcb2ea29
Gustavo Cunha <github@gustavocunha.dev>
2019-10-27 16:01:30 +0100

    [SHAME] This commit fixes a code that would never work.
    The excess of confidence and hurry on delivering code makes a developer push code without proper testing. Fortunately, such code would never reach production because automated test suite was good enough to capture it before hand.
    
    This is a constant reminder to future me that, regardless of experience and knowledge in a language or domain, I won't make such mistakes.

--- app/listeners/reorder_listener.rb
+++ app/listeners/reorder_listener.rb
@@ -16,16 +16,15 @@ class ReorderListener < BaseListener
   private
 
   def reorder(objects, event)
-    payload = {}
     objects.each do |object|
       index = ids.index(object.id)
       object.update(order: index)
-      publish(event, payload(object))
+      publish(event, payload_from(object))
     end
   end
 
-  def payload(object)
-    payload[hash_key(object)] = object
+  def payload_from(object)
+    Hash[hash_key(object), object]
   end
 
   def hash_key(object)
