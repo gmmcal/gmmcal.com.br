f232e88cf6eb7e6d1a7280fbf7bc0e73619952c8
Gustavo Cunha <github@gustavocunha.dev>
2020-01-03 19:09:02 +0100

    Add United States as a list of countries

--- spec/backend/lib/country_spec.rb
+++ spec/backend/lib/country_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Country, type: :library do
     I18n.locale = :en
   end
 
-  it do
-    expect(described_class.all).to eq [%w[Brazil BR], %w[Netherlands NL], %w[Portugal PT]]
+  it 'returns all countries with their ISO code' do
+    expect(described_class.all).to eq [%w[Brazil BR], %w[Netherlands NL], %w[Portugal PT], ['United States', 'US']]
   end
 end
