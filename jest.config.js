146af460dfa83736816fd175368e22f0317233dc
Gustavo Cunha <github@gustavocunha.dev>
2020-02-15 15:57:40 +0100

    Fix Jest and deploy configuration

--- jest.config.js
+++ jest.config.js
@@ -22,8 +22,8 @@ module.exports = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    "app/javascript/packs/components/**/*.{js,vue}",
-    "app/javascript/packs/functional/**/*.{js}",
+    "app/javascript/packs/components/**/*.{js,vue,}",
+    "app/javascript/packs/functional/**/*.{js,}",
     "!**/node_modules/**"
   ],
 
@@ -38,7 +38,8 @@ module.exports = {
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: [
     "lcovonly",
-    "html"
+    "html",
+    "text-summary"
   ],
 
   // An object that configures minimum threshold enforcement for coverage results
