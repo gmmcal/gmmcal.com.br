b8940ae680880c63d1f601c6cddcd6289201983e
Gustavo Cunha <github@gustavocunha.dev>
2020-06-03 21:26:11 +0200

    Update dependencies

--- spec/backend/rails_helper.rb
+++ spec/backend/rails_helper.rb
@@ -7,9 +7,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'support/simplecov'
 require File.expand_path('../../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-if Rails.env.production?
-  abort('The Rails environment is running in production mode!')
-end
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'factory_bot_rails'
