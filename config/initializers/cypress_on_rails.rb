8f168904156e0793b1e9e19b8cb492f2a23aa1b5
Gustavo Cunha <github@gustavocunha.dev>
2020-07-12 11:53:03 +0200

    Use cypress middleware

--- config/initializers/cypress_on_rails.rb
+++ config/initializers/cypress_on_rails.rb
@@ -5,7 +5,7 @@ if defined?(CypressOnRails)
     c.cypress_folder = File.expand_path("#{__dir__}/../../spec/end-to-end")
     # WARNING!! CypressOnRails can execute arbitrary ruby code
     # please use with extra caution if enabling on hosted servers or starting your local server on 0.0.0.0
-    c.use_middleware = Rails.env.test?
+    c.use_middleware = !Rails.env.production?
     c.logger = Rails.logger
   end
 end
