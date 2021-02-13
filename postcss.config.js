50a5acc133ed65a9619689683981763b420718c4
Gustavo Cunha <github@gustavocunha.dev>
2021-02-13 23:10:11 +0100

    Bump tailwind to latest version

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,6 @@
-const tailwindcss = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-  ],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-  whitelist: [],
-  whitelistPatternsChildren: [/skill$/, /flag$/, /banner-img$/, /topmar$/]
-})
-
 module.exports = {
   plugins: [
-    tailwindcss('app/javascript/packs/tailwind.config.js'),
-    require('autoprefixer'),
+    require('tailwindcss')('app/javascript/packs/tailwind.config.js'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -20,7 +8,6 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    }),
-    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
+    })
   ]
 }
