86ce062bd6941c2e7378b87fdd36ceceecd6e914
Gustavo Cunha <dev@gustavocunha.dev>
2024-02-24 07:49:38 +0100

    Bump ruby to v3.3, Node to v20 and yarn to v4

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+
 # frontend
 pin 'application'
 pin 'frontend/icons'
@@ -17,8 +18,8 @@ pin 'admin/application'
 pin 'admin/components/modal'
 pin 'admin/components/sortable'
 
-pin '@fortawesome/fontawesome-svg-core', to: 'https://ga.jspm.io/npm:@fortawesome/fontawesome-svg-core@6.4.2/index.mjs'
-pin '@fortawesome/free-brands-svg-icons', to: 'https://ga.jspm.io/npm:@fortawesome/free-brands-svg-icons@6.4.2/index.mjs'
-pin '@fortawesome/free-regular-svg-icons', to: 'https://ga.jspm.io/npm:@fortawesome/free-regular-svg-icons@6.4.2/index.mjs'
-pin '@fortawesome/free-solid-svg-icons', to: 'https://ga.jspm.io/npm:@fortawesome/free-solid-svg-icons@6.4.2/index.mjs'
-pin 'sortablejs', to: 'https://ga.jspm.io/npm:sortablejs@1.15.0/modular/sortable.esm.js'
+pin '@fortawesome/fontawesome-svg-core', to: 'https://ga.jspm.io/npm:@fortawesome/fontawesome-svg-core@6.5.1/index.mjs'
+pin '@fortawesome/free-brands-svg-icons', to: 'https://ga.jspm.io/npm:@fortawesome/free-brands-svg-icons@6.5.1/index.mjs'
+pin '@fortawesome/free-regular-svg-icons', to: 'https://ga.jspm.io/npm:@fortawesome/free-regular-svg-icons@6.5.1/index.mjs'
+pin '@fortawesome/free-solid-svg-icons', to: 'https://ga.jspm.io/npm:@fortawesome/free-solid-svg-icons@6.5.1/index.mjs'
+pin 'sortablejs', to: 'https://ga.jspm.io/npm:sortablejs@1.15.2/modular/sortable.esm.js'
